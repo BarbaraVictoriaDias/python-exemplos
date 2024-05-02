6cbb7fb23724170bf17d84ffe1b162e296ec4e6b
BarbaraVictoriaDias <babydias2007@gmail.com>
2024-05-02 16:30:32 -0300

    Criação fstrings2.py

--- fstrings.py
+++ fstrings.py
@@ -1,7 +1,7 @@
 #Formatação de Strings
 from datetime import datetime
 ano_atual = datetime.now().year
-clube = SPFC
+clube = 
 ano_fundacão = 1900
 campeonato_mundial = 4
 print(f"{clube} possui {campeonato_mundial}títulos mundiais.")
