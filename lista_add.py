e7edaa0c00bb5b2aec717114a12378f1ba920d21
BarbaraVictoriaDias <babydias2007@gmail.com>
2024-05-21 15:11:56 -0300

    Create lista_add.py

---
+++ lista_add.py
@@ -0,0 +1,9 @@
+alunos = ['Grenda']
+alunos.append('Laurão')
+while True:
+    nome = input('Digite o nome do aluno: ')
+    alunos.append(nome)
+    resposta = input('Deseja adicionar mais um aluno? (S/N): ')
+    if resposta.upper() == 'N':
+        break
+print(f"Alunos cadastrados: {alunos}")
