216a8eed9889786b2235c28c7cfcc15cd7ca2f68
BarbaraVictoriaDias <babydias2007@gmail.com>
2024-05-23 10:12:13 -0300

    create copia.py

--- lista_remover.py
+++ lista_remover.py
@@ -6,3 +6,9 @@ print(f"Lista de alunos: {alunos}")
 # Método pop()
 alunos.pop(2)
 print(f"Lista de alunos: {alunos}")
+# Função del
+del alunos[3]
+pint(f"Lista de alunos: {alunos}")
+# Metodo clear()
+aluno.clear()
+print(f"Lista de alunos: {alunos}")
