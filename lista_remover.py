37d5bab03af3fddec36710136564833bd0d03acb
BarbaraVictoriaDias <babydias2007@gmail.com>
2024-05-23 09:29:35 -0300

    Create remover.py

---
+++ lista_remover.py
@@ -0,0 +1,8 @@
+alunos=['Adriana', 'João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana', 'Adriana']
+print(f"Lista de alunos {alunos}")
+# Método remove()
+alunos.remove('Adriana')
+print(f"Lista de alunos: {alunos}")
+# Método pop()
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
